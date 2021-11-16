b98f70fe4bb1155e3ab3b6927cdce743249b1047
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-17 00:38:19 +0300

    put function

--- app.js
+++ app.js
@@ -19,7 +19,35 @@ fs.readFile("./todos.json", (err, data) => {
   }
 });
 
+/////// put function to make changes on data (update)
+app.put("/todos", (req,res)=>{
+    const {id,task,isCompleted, isDel} = req.body
+    let check = false;
 
+   todos.forEach((todo)=>{
+       if (todo.id == id) {
+        if (task != undefined){
+            todo.task = task;
+        } else if(isCompleted != undefined){
+            todo.isCompleted = isCompleted
+        } else if (isDel != undefined){
+            todo.isDel= isDel
+        }
+        
+        check = true;
+    } })
+    if (check){
+        fs.writeFile("./todos.json", (err,res)=>{
+            if(err){
+                return err.message
+            }
+            else {
+                res.status(200).json(todos)
+            }
+        })
+    } else { res.status(404).json("no such a task")}
+
+})
 
 //// GET/READ the data to the router 
 app.get("/todos", (req,res)=>{
