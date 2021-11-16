42a6f36f774f8ce20f7a90d8d0508f80dfcdc2bb
lamalajaji <Lama.Mohammed.elq.js@tuwaiq.edu.sa>
2021-11-16 23:50:19 +0300

    add + read fs file data

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const fs = require("fs");
 
-
-
-const PORT =  4000;
-
-
+const PORT = 4000;
 
 //// app level middleware
 app.use(express.json());
 
-app.listen(PORT, ()=>{
-    console.log(`this server started on ${PORT}`);
-})
+let todos = [];
+//// read function to read the data from json file
+fs.readFile("./todos.json", (err, data) => {
+  if (err) {
+    return err.message;
+  } else {
+    todos = JSON.parse(data.toString());
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`this server started on ${PORT}`);
+});
